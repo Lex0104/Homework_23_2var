546701c52566a5ef3d075ff61a80c8e4b6c64562
Eduard <barned85@gmail.com>
2025-02-25 23:07:55 +0300

    add form.py
    delete product.image | media_filter
    
    add migration in blog

---
+++ myproject/blog/templates/blog/form.py
@@ -0,0 +1,26 @@
+
+from django.forms.models import ModelForm
+from myproject.blog.models import Blog
+
+
+class BlogForm(ModelForm):
+    class Meta:
+        model = Blog
+        fields = {"title", "content", "preview", "flag_publication"}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields["title"].widget.attrs.update({
+            'placeholder': 'Введите заголовок блога',
+            'class': "form-control"
+        })
+
+        self.fields["content"].widget.attrs.update({
+            'placeholder': "Напишите содержимое блога",
+            'class': "form-control"
+        })
+
+        self.fields["preview"].widget.attrs.update({
+            'class': "form-control"
+        })
