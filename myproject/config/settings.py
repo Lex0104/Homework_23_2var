913600e4e9063511ea65126fdac4784fcc33a59c
Eduard <barned85@gmail.com>
2025-02-25 19:16:35 +0300

    fixes base path css and js

--- myproject/config/settings.py
+++ myproject/config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-zgpj$*lt7jhpxtehhijc@k4q8%5lx72zvc5ii3j)_6grie-*hp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.getenv('DEBUG') == "True" else False
